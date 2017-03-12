20fe8c1f36b783daea84bae05a92cc74b69fe4ab
Obranan <www.dimon20@email.ua>
2017-03-12 22:51:34 +0200

    update

--- js/common.js
+++ js/common.js
@@ -1,78 +1,65 @@
 "use strict";
 
-// создаём окошко с ошибкой
+// создаём окошко с сообщением
 var form = document.querySelector(".form");
 var divError = document.createElement("div");
-var errorTimer = 5000;
+divError.id = "pop-up-window";
+const ERROR_TIMER = 5000;	// время через которое удалиться сообщение error
 
 function messageError(message) {
-	divError.setAttribute("data-error", "error");
-	divError.innerHTML = message;
-	divError.style.display = "block";
-	form.appendChild(divError);
+	if(message) {
+		divError.innerHTML = message;
+	}
+	messageOpen();
 	setTimeout( () => {
 		divError.style.display = "none";
-	}, errorTimer);
+	}, ERROR_TIMER);
 }
 
-// окошко для введённых значений в инпуты
-var buttonOk = document.createElement("button");
-var buttonNo = document.createElement("button");
-
-function messageInfo(message) {
-	divError.setAttribute("data-error", "error");
-	divError.innerHTML = message;
+function messageOpen() {
+	divError.setAttribute("data-info", "info");
 	divError.style.display = "block";
-
-	buttonOk.innerHTML = "ок";
-	buttonOk.classList.add("submit", "ok");
-	divError.appendChild(buttonOk);
-
-	buttonNo.innerHTML = "отмена";
-	buttonNo.classList.add("submit", "no");
-	divError.appendChild(buttonNo);
-
 	form.appendChild(divError);
 }
-
-// проверяем что введено число
-function inputValue(inputID) {
-	if(isNaN(inputID)) {
-		messageError("Ведите число!"); 
-	}
-	return inputID;
+// Если введено значение, label не возвращаеться обратно
+function noValueLabelStop(link, value) {
+	var selectLink = document.querySelectorAll(link);
+	selectLink.forEach((elem) => {
+		if(!(value == "") && !(value == NaN)) {
+			elem.classList.add("out-of-focus");
+		}else {
+			elem.classList.remove("out-of-focus");
+		}
+	})
 }
-
-// проверяем что введённое значение не более number
-function notMoreValue(inputID, number, message) {
-	if(inputID > number) {
+// проверяем что введено число
+function inputValue(value) {
+	if(isNaN(value)) {
+		messageError("Введите число!"); 
+	}
+	return value;
+}
+// проверяем что введённое значение не более заданного числа
+function notMoreValue(value, number, message) {
+	if(value > number) {
 		messageError(message);
 	}
-	return inputID;
-}
-// узнаём выбранный radio button
-var selectedValue;
-
-function radioValue(inputRadioName) {
-	for (var i = 0; i < inputRadioName.length; i++) {
-		if (inputRadioName[i].checked) {
-			return selectedValue = inputRadioName[i].value;
-		}
-	}	
+	return value;
 }
 
-// если переключаем фокус выводит сообщение об ошибке
+// Когда теряем фокус
 var input = document.querySelectorAll("input[type=text]");
-for(var i = 0; i < input.length; i++) {
-	var allInput = input[i];
+input.forEach((item) => {
+	var allInput = item;
 	allInput.addEventListener("blur", notFocus);
-}
+});
 
 var passengersValue;	// пассажиры
 var linenValue;			// Постельное бельё
 var teaValue;			// чай
 var dinnerValue;		// обед
 var distanceValue;		// расстояние
+const NUMBER_TEA = 14;		// количество чая
 
 function notFocus() {
 	passengersValue = +passengers.value;
@@ -85,17 +72,25 @@ function notFocus() {
 	distanceValue = inputValue(distanceValue);
 
 	linenValue = notMoreValue(inputValue(linenValue), passengersValue, "Постельного белья не должно быть больше количества пассажиров!");
-	teaValue = notMoreValue(inputValue(teaValue), 20, "Чая не больше 20 шт!");
+	teaValue = notMoreValue(inputValue(teaValue), NUMBER_TEA, "Чая не больше " + NUMBER_TEA + " шт!");
 	dinnerValue = notMoreValue(inputValue(dinnerValue), passengersValue, "Обедов не должно быть больше количества пассажиров!");
+
+	noValueLabelStop("#passengers + .label-text", passengersValue);
+	noValueLabelStop("#linen + .label-text", linenValue);
+	noValueLabelStop("#tea + .label-text", teaValue);
+	noValueLabelStop("#dinner + .label-text", dinnerValue);
+	noValueLabelStop("#distance + .label-text", distanceValue);
 }
-
-// при клике на button
+// Отправляем форму
 var submit = document.querySelector(".submit");
-
 function submitValueForm(event) {
-	// находим свойство и значение - типа вагона
 	var radioName = document.getElementsByName("wagon");
-	var radioName = radioValue(radioName);
+	// узнаём выбранный radio button
+	radioName.forEach((elem)  => {
+		if (elem.checked) {
+			radioName = elem.value;
+ 		}
+	});
 	var objRadio = {
 	  SV: 44,
 	  Cupe: 32,
@@ -105,48 +100,98 @@ function submitValueForm(event) {
 	if(radioName in objRadio) {
 		var selectedRadioValue = objRadio[radioName];
 	}
-
-	var sumLinens = 4; // Сумма белья
-	var sumTea = 2; // Сумма чая
-	var sumDinner = 5; // Сумма обеда
-	var sumMileage = 0.10;  // Сумма километража
-	var numberDiscount = 0.2; // Скидка 20%
-
+	const SUM_LINENS = 4; // Сумма белья
+	const SUM_TEA = 2; // Сумма чая
+	const SUM_DINNER = 5; // Сумма обеда
+	const SUM_MILEAGE = 0.10;  // Сумма километража
+	const NUMBER_DISCOUNT = 0.2; // Скидка 20%
 	// сумма
-	var total = (passengersValue * selectedRadioValue) + (linenValue * sumLinens) + (teaValue * sumTea) + (dinnerValue * sumDinner) + (distanceValue * sumMileage);
-	// сообщение "итого"
-	messageInfo(`<h2>Подтвердите заказ!</h2> <p>Количество пассажиров: ${passengersValue} чел</p> <p>Количество комплектов постельного белья: ${linenValue} шт</p> <p>Тип вагона: ${radioName} </p> <p>Количество чая: ${teaValue} шт</p> <p>Количество обедов: ${dinnerValue} шт</p> <p>Расстояние между городами: ${distanceValue} км</p> <p>Всего к оплате -  ${total}  грн</p>`);
-
+	var total = (passengersValue * selectedRadioValue) + (linenValue * SUM_LINENS) + (teaValue * SUM_TEA) + (dinnerValue * SUM_DINNER) + (distanceValue * SUM_MILEAGE);
+	// создаёт сообщение с "подтверждением заказа"
+	function ConfirmOrder() {
+		var listTemplateConfirm = document.getElementById("list-template-confirm").innerHTML;
+		var template = Handlebars.compile(listTemplateConfirm);
+		var listContent = template({
+			name: "Подтвердите заказ!",
+			list: [
+				{item: `Количество пассажиров: ${passengersValue} чел`},
+				{item: `Количество комплектов постельного белья: ${linenValue} шт`},
+				{item: `Тип вагона: ${radioName}`},
+				{item: `Количество чая: ${teaValue} шт`},
+				{item: `Количество обедов: ${dinnerValue} шт`},
+				{item: `Расстояние между городами: ${distanceValue} км`},
+				{item: `Всего к оплате -  ${total}  грн`}
+			]
+		});
+		divError.innerHTML = listContent;
+		messageOpen();
+	}
+	ConfirmOrder();
 	// клик по кнопке ok
 	var ok = document.querySelector(".ok");
-	function clickOn(event) {
+	function paymentOk(event) {
 		messageError(`Оплата подтверждена -  ${total}  грн`);
 	}
-	ok.addEventListener("click", clickOn);
-
+	ok.addEventListener("click", paymentOk);
 	// клик по кнопке no
 	var no = document.querySelector(".no");
-	function clickNo(event) {
-		messageInfo("Вы можете приобрести данные услуги со скидкой 20%");
+	function paymenNo(event) {
 
-		// второй клик по кнопке ok
-		function clickOk() {
-			var discount = Math.round(total * numberDiscount);
+		function discountMessage() {
+			var listTemplateConfirm = document.getElementById("list-template-confirm").innerHTML;
+			var template = Handlebars.compile(listTemplateConfirm);
+			var listContent = template({
+				name: "Вы можете приобрести данные услуги со скидкой 20%!"
+			});
+			divError.innerHTML = listContent;
+			messageOpen();
+		}
+		discountMessage();
+		// если согласен на скидку
+		var ok = document.querySelector(".ok");
+		function discountOk() {
+			var discount = Math.round(total * NUMBER_DISCOUNT);
 			var totalDiscount = Math.round(total - discount);
-			messageError(`<p>Скидка составила - ${discount} грн</p> <p>К оплате услуг со скидкой состовляет - ${totalDiscount}  грн</p>`);
+			// создаёт сообщение с "подтверждением заказа со скидкой"
+			function discountMessageTotal() {
+				var listTemplateAlert = document.getElementById("list-template-alert").innerHTML;
+				var templateAlert = Handlebars.compile(listTemplateAlert);
+				var listContent = templateAlert({
+					name: "Оплата подтверждена!",
+					list: [
+						{item: `Скидка составила - ${discount} грн`},
+						{item: `К оплате услуг со скидкой состовляет - ${totalDiscount}  грн`}
+					]
+				});
+				divError.innerHTML = listContent;
+				messageError();
+			}
+			discountMessageTotal();
 		}
-		ok.addEventListener("click", clickOk);
+		ok.addEventListener("click", discountOk);
+		// если не согласен на скидку
+		var no = document.querySelector(".no");
+		function discountNo() {
 
-		// второй клик по кнопке no
-		function clickNo() {
-			messageError("<p>Ваш заказ удален!</p> <p>Досвидания!</p>");
+			function MessageClosePayment() {
+				var listTemplateAlert = document.getElementById("list-template-alert").innerHTML;
+				var templateAlert = Handlebars.compile(listTemplateAlert);
+				var listContent = templateAlert({
+					name: "Ваш заказ удален!",
+					list: [
+						{item: `Досвидания!`}
+					]
+				});
+				divError.innerHTML = listContent;
+				messageError();
+			}
+			MessageClosePayment();
+			
 		}
-		no.addEventListener("click", clickNo);
-
+		no.addEventListener("click", discountNo);
 		event.preventDefault();
 	}
-	no.addEventListener("click", clickNo);
-
+	no.addEventListener("click", paymenNo);
 	event.preventDefault();
 }
 submit.addEventListener("click", submitValueForm);
